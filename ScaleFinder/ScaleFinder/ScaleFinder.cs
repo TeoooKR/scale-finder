cb16d5575638f9cc2cd3449cd52e84716a8fa366
Teo Han <meteory.kr@gmail.com>
2023-02-14 21:01:31 +0900

    Naming and Bug Fixed

--- ScaleFinder/ScaleFinder/ScaleFinder.cs
+++ ScaleFinder/ScaleFinder/ScaleFinder.cs
@@ -2,6 +2,17 @@
 {
     class ScaleFinder
     {
+        //------ TYPE ------
+        public const int type_MAJOR = 1;
+        public const int type_MINOR = 6;
+        public const int type_IONIAN = 1;
+        public const int type_DORIAN = 2;
+        public const int type_PHTYGIAN = 3;
+        public const int type_LYDIAN = 4;
+        public const int type_MIXOLYDIAN = 5;
+        public const int type_AEOLIAN = 6;
+        public const int type_LOCRAIN = 7;
+
         //------ PITCH ------
         readonly public static int pitch_C = 1;
         readonly public static int pitch_D = 3;
@@ -16,20 +27,10 @@
         readonly public static int accid_SHARP = 1;
         readonly public static int accid_FLAT = -1;
 
-        //------ TYPE ------
-        public const int type_MAJOR = 1;
-        public const int type_MINOR = 6;
-        public const int type_IONIAN = 1;
-        public const int type_DORIAN = 2;
-        public const int type_PHTYGIAN = 3;
-        public const int type_LYDIAN = 4;
-        public const int type_MIXOLYDIAN = 5;
-        public const int type_AEOLIAN = 6;
-        public const int type_LOCRAIN = 7;
+        readonly private int[] BasePitchList = new int[13] { 1, 3, 5, 6, 8, 10, 12, 13, 15, 17, 18, 20, 22 };
+        readonly private string[] BasePitchTextList = new string[7] { "C", "D", "E", "F", "G", "A", "B" };
 
-        readonly private int[] def_pitch_index = new int[7] { 1, 3, 5, 6, 8, 10, 12 };
-        readonly private string[] sc = new string[7] { "C", "D", "E", "F", "G", "A", "B" };
-
+        //------ MODE's Interval -------
         readonly private int[] ionian_interval = new int[6] { 2, 2, 1, 2, 2, 2 };
         readonly private int[] dorian_interval = new int[6] { 2, 1, 2, 2, 2, 1 };
         readonly private int[] phtygian_interval = new int[6] { 1, 2, 2, 2, 1, 2 };
@@ -38,115 +39,90 @@
         readonly private int[] aeolian_interval = new int[6] { 2, 1, 2, 2, 1, 2 };
         readonly private int[] locrain_interval = new int[6] { 2, 1, 2, 1, 2, 2 };
 
-        public int FindScale(int start_pitch, int accid, int type)
+        public int FindScale(int basePitch, int accidental, int type)
         {
-            int[] sc_index = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
-            int[] accidental = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
-            int[] accidental_index = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+            int[] pitchList = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+            int[] accidentalList = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
             string[] sub_sc = new string[7] { "", "", "", "", "", "", "" };
             string[] accidental_string = new string[7] { "", "", "", "", "", "", "" };
             string[] fin_sc = new string[7] { "", "", "", "", "", "", "" };
-            int pitch_index = start_pitch + accid;
+            int startPitch = basePitch + accidental;
 
             if (type == type_MAJOR || type == type_IONIAN)
             {
-                get_type_interval(sc_index, pitch_index, ionian_interval);
+                GetPitchListByInterval(pitchList, startPitch, ionian_interval);
             }
             else if (type == type_DORIAN)
             {
-                get_type_interval(sc_index, pitch_index, dorian_interval);
+                GetPitchListByInterval(pitchList, startPitch, dorian_interval);
             }
             else if (type == type_PHTYGIAN)
             {
-                get_type_interval(sc_index, pitch_index, phtygian_interval);
+                GetPitchListByInterval(pitchList, startPitch, phtygian_interval);
             }
             else if (type == type_LYDIAN)
             {
-                get_type_interval(sc_index, pitch_index, lydian_interval);
+                GetPitchListByInterval(pitchList, startPitch, lydian_interval);
             }
             else if (type == type_MIXOLYDIAN)
             {
-                get_type_interval(sc_index, pitch_index, mixolydian_interval);
+                GetPitchListByInterval(pitchList, startPitch, mixolydian_interval);
             }
             else if (type == type_AEOLIAN || type == type_MINOR)
             {
-                get_type_interval(sc_index, pitch_index, aeolian_interval);
+                GetPitchListByInterval(pitchList, startPitch, aeolian_interval);
             }
             else if (type == type_LOCRAIN)
             {
-                get_type_interval(sc_index, pitch_index, locrain_interval);
+                GetPitchListByInterval(pitchList, startPitch, locrain_interval);
             }
 
-            for (int i = 0; i < def_pitch_index.Length; i++)
+            for (int i = 0; i < BasePitchList.Length; i++)
             {
-                if (start_pitch == def_pitch_index[i])
+                if (basePitch == BasePitchList[i])
                 {
-                    accidental_index = get_accidental_index(accidental, i, sc_index);
-                    sub_sc = get_sc_order(sc, i);
+                    CalcAccidentalFromBass(accidentalList, i, pitchList);
+                    sub_sc = get_sc_order(BasePitchTextList, i);
+                    break;
                 }
             }
 
             //for (int i = 0; i < accidental_index.Length; i++)
             //{}
 
-            int result = pitch_index;
+            int result = startPitch;
             Console.WriteLine("I Found Scale!!!!!!!!!!!!!!!!");
             return result;
         }
 
-        private int get_type_interval(int[] sc_index, int pitch_index, int[] interval)
+        private void GetPitchListByInterval(int[] result, int startPitch, int[] interval)
         {
-            sc_index[0] = pitch_index;
-            Console.WriteLine("index = " + sc_index[0]);
+            result[0] = startPitch;
+            Console.WriteLine("index = " + result[0]);
             for (int i = 0; i < interval.Length; i++)
             {
-                sc_index[i + 1] = sc_index[i] + interval[i];
-                Console.WriteLine("index = " + sc_index[i + 1]);
+                result[i + 1] = result[i] + interval[i];
+                Console.WriteLine("index = " + result[i + 1]);
             }
-            return 1;
         }
 
-        private int[] get_accidental_index(int[] accidental, int start_pitch_index, int[] sc_index)
+        private void CalcAccidentalFromBass(int[] accidental, int startPitchIndex, int[] pitchList)
         {
             //> pitch_order_index [0~6]
-            int j = 0;
-            if (start_pitch_index == 0)
+            int index = 0;
+            for (int i = 0; i < pitchList.Length; i++)
             {
-                for (int i = 0; i < def_pitch_index.Length; i++)
-                {
-                    Console.WriteLine("---- " + i + " ---- " + sc_index[i] + " / " + def_pitch_index[i]);
-                    accidental[i] = sc_index[i] - def_pitch_index[i];
-                    Console.WriteLine(accidental[i]);
-                }
-                return accidental;
-            }
-
-            for (int i = 0; i < def_pitch_index.Length; i++)
-            {
-                j = i + start_pitch_index;
-                if (j > 6)
-                {
-                    j = j % 6;
-                }
-                if (sc_index[i] >= 12)
-                {
-                    sc_index[i] = sc_index[i] % 12;
-                }
-                Console.WriteLine("---- " + i + " ---- " + sc_index[i] + " / " + def_pitch_index[i]);
-                accidental[i] = sc_index[i] - def_pitch_index[j];
-                if (accidental[i] >= 12)
-                {
-                    accidental[i] = accidental[i] % 12;
-                }
+                index = i + startPitchIndex;
+                Console.WriteLine("---- " + i + " ---- " + pitchList[i] + " / " + BasePitchList[index]);
+                accidental[i] = pitchList[i] - BasePitchList[index];
                 Console.WriteLine(accidental[i]);
             }
-            return accidental;
         }
-
 
         private string[] get_sc_order(string[] sc, int pitch_order_index)
         {
             string[] result = { "C", "D", "E", "F", "G", "A", "B" };
+
             return result;
         }
     }
