17e5a6a9100bc52e4803365218ea0da0b7cae3b2
Teo Han <meteory.kr@gmail.com>
2023-02-15 20:18:40 +0900

    Add Function

--- ScaleFinder/ScaleFinder/Program.cs
+++ ScaleFinder/ScaleFinder/Program.cs
@@ -8,7 +8,7 @@ namespace ScaleFinder
         int find = 0;
         static void Main(string[] args)
         {
-            int result = finder.FindScale(ScaleFinder.pitch_D, 1330, ScaleFinder.type_MAJOR);
+            int result = finder.FindScale(ScaleFinder.pitch_C, ScaleFinder.accid_NATURAL, ScaleFinder.type_MAJOR);
             //Console.WriteLine(">> MUSIC SCALE : " + result);
         }
     }
