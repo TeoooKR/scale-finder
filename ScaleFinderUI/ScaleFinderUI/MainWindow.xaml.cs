05aeb513bdfa91ddaebfc356bf9afd60e9743fe3
Teo Han <meteory.kr@gmail.com>
2023-08-02 22:10:28 +0900

    Add Scale result

--- ScaleFinderUI/ScaleFinderUI/MainWindow.xaml.cs
+++ ScaleFinderUI/ScaleFinderUI/MainWindow.xaml.cs
@@ -21,36 +21,103 @@ namespace ScaleFinderUI {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        private int BasePitch = ScaleFinder.PitchC;
+        private int Accid = ScaleFinder.AccidNatural;
+        private string BasePitchText = "C";
+        private string AccidText = String.Empty;
 
-        static ScaleFinder finder = new();
+        static ScaleFinder Finder = new();
         public MainWindow() {
+            this.Loaded += new RoutedEventHandler(WindowLoaded);
             InitializeComponent();
         }
-
+        private void WindowLoaded(object sender, RoutedEventArgs e) {
+            RBtnBaseC.IsChecked = true;
+            RBtnAccidN.IsChecked = true;
+        }
         private void HandleBasePitchChecked(object sender, RoutedEventArgs e) {
             RadioButton tb = sender as RadioButton;
-            int basePitch = 0;
-            
             if (tb == null) {
                 return;
             }
             if ((bool)RBtnBaseC.IsChecked) {
-                basePitch = ScaleFinder.PitchC;
+                BasePitch = ScaleFinder.PitchC;
             }
             else if ((bool)RBtnBaseD.IsChecked) {
-                basePitch = ScaleFinder.PitchD;
+                BasePitch = ScaleFinder.PitchD;
+            }
+            else if ((bool)RBtnBaseE.IsChecked) {
+                BasePitch = ScaleFinder.PitchE;
+            }
+            else if ((bool)RBtnBaseF.IsChecked) {
+                BasePitch = ScaleFinder.PitchF;
+            }
+            else if ((bool)RBtnBaseG.IsChecked) {
+                BasePitch = ScaleFinder.PitchG;
+            }
+            else if ((bool)RBtnBaseA.IsChecked) {
+                BasePitch = ScaleFinder.PitchA;
+            }
+            else if ((bool)RBtnBaseB.IsChecked) {
+                BasePitch = ScaleFinder.PitchB;
             }
 
-            TBSelectedScale.Text = (string)tb.Content;
-
-            Scale result = finder.FindScale(basePitch, ScaleFinder.AccidNatural, ScaleFinder.ModeMajor);
+            if (TBSelectedScale == null) {
+                return;
+            }
+            BasePitchText = (string)tb.Content;
+            TBSelectedScale.Text = BasePitchText + AccidText;
+            Scale result = Finder.FindScale(BasePitch, Accid, ScaleFinder.ModeMajor);
 
             if (!result.GetFound()) {
                 Debug.WriteLine("Error.. I cannot find your scale. sigh....");
                 return;
             }
-            result.PrintMyValues();
 
+            UpdateScaleResult(result.GetPitchTexts());
+            result.PrintMyValues();
+        }
+        private void HandleAccidChecked(object sender, RoutedEventArgs e) {
+            RadioButton rb = sender as RadioButton;
+
+            if (rb == null) {
+                return;
+            }
+            if ((bool)RBtnAccidN.IsChecked) {
+                Accid = ScaleFinder.AccidNatural;
+                AccidText = "";
+            }
+            else if ((bool)RBtnAccidS.IsChecked) {
+                Accid = ScaleFinder.AccidSharp;
+                AccidText = "#";
+            }
+            else if ((bool)RBtnAccidF.IsChecked) {
+                Accid = ScaleFinder.AccidFlat;
+                AccidText = "♭";
+            }
+
+            if (TBSelectedScale == null) {
+                return;
+            }
+            TBSelectedScale.Text = BasePitchText + AccidText;
+
+            Scale result = Finder.FindScale(BasePitch, Accid, ScaleFinder.ModeMajor);
+
+            if (!result.GetFound()) {
+                Debug.WriteLine("Error.. I cannot find your scale. sigh....");
+                return;
+            }
+
+            UpdateScaleResult(result.GetPitchTexts());
+            result.PrintMyValues();
+        }
+
+        private void UpdateScaleResult(string[] texts) {
+            string resultText = "";
+            for (int i = 0; i < texts.Length; i++) {
+                resultText += texts[i] + " ";
+            }
+            TBScaleResult.Text = resultText;
         }
     }
 }
